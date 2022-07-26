ed94f706f9c01150bc3f6813b8175bf9fba19f6c
Tobias Allrich <tobias@arbeit.de>
2022-07-26 15:11:39 +0200

    Rewrote view section to multiprocessing pyqtgraph Deleted classes.py and moved processes in main.py

--- main.py
+++ main.py
@@ -1,48 +1,87 @@
-from multiprocessing import Process
-from threading import Thread
 import sys
-import socket
-import multiprocessing as mp
-from classes import *
+import argparse
+import numpy as np
+import serial
+from numba import jit
+import re
+import pyqtgraph as pg
+import pyqtgraph.multiprocess as mp
+from tqdm import tqdm
+app = pg.mkQApp('test')
 
-def view():
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind(('', 2000))
-    g = Graph(4)
-    while True:
-        try:
-            message = server.recv(4096).decode('utf-8')
-            g.addData([float(x) for x in message[1:].split(',')])
-            if 'exit' in message:
-                return
-            print(message)
-        except Exception as e:
+#CONSTANTS
+PATTERN = r'[-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+)'
+PATTERN_ALT = r'[-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+), [-+]?(\d*\.\d+|\d+)'
+
+#FUNCTIONS
+def update(readout) -> None:
+    global rr, curves, points
+    try:
+        for s in readout:
+            if re.fullmatch(PATTERN, s) or re.fullmatch(PATTERN_ALT, s):
+                data = [float(x) for x in s.split(',')]
+                for i,y in enumerate(data):          
+                    if len(rr[i]) >points[i]:
+                        rr[i].pop(0)
+                    rr[i].append(y)
+                    curves[i].setData(y=rr[i], _callSync='off')
+                #print(message)              
+    except Exception as e:
             print('[view] ' + str(e))
 
-def reader(print_address, port, baud):
-    try:
-        conn = Conn(port=port, baud=baud)
-        #print_address = ('127.0.0.1', 2000)   
-        print_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except Exception as e:
-        print(e)
+def mean_update(message):
+    global rr, curves, points
+    data = np.nanmean(message, axis=0)
+    for i,y in enumerate(data):          
+        if len(rr[i]) >points[i]:
+            rr[i].pop(0)
+        rr[i].append(y)
+        curves[i].setData(y=rr[i], _callSync='off')
+
+def adder(message):
+    return np.array([x.split(',') + (5-len(x.split(',')))*['nan'] for x in message]).astype(float)
+    
+
+#PARSER
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--port', '--p', required=True)
+# parser.add_argument('--baud', '--b', required=True)
+# parser.add_argument('--limit', '--l', required=True)
+# parser.add_argument('--points', required=False)
+# args = parser.parse_args()
+
+try:
+    #WINDOW
+    view = mp.QtProcess()
+    rpg = view._import('pyqtgraph')
+    win = rpg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+    lim = 5#int(args.limit)
+    points = [200,200,200,300,75]
+    rpg.setConfigOptions(antialias=True)
+    plots = []
+    curves = []
+    rr = []
+    for i in range(lim):
+        rr.append([])
+        plots.append(win.addPlot(title='titles'))
+        plots[i].enableAutoRange('xy', True)
+        curves.append(plots[i].plot(pen='y'))
+        #if i % 3 == 2:
+        win.nextRow()
+    print('[view] parameters set')        
+    #READER
+    conn = serial.Serial(port='COM3', baudrate=500000, timeout=0.01)
+    print('[main] established connection')
+    readbuffer = ''
+    print('[main] socket connected')
+    i = 0
     while True:
-        try:
-            if conn.is_open:
-                for s in conn.read().split('\n\r'):
-                    if s != '':
-                        print_socket.sendto(s.encode('utf-8'), print_address)
-        except Exception as e:
-            print('[inputhandler-map]' + str(e))
-
-if __name__ == '__main__':
-    port = sys.argv[0] if len(sys.argv) == 2 else 'COM3'
-    baud = sys.argv[1] if len(sys.argv) == 2 else 500000
-
-    inp = Process(target=reader, args=(('127.0.0.1', 2000), port, baud))
-    outp = Process(target=view)
-
-    inp.start()
-    outp.start()
-    inp.join()
-    outp.join()
+        if conn.is_open:
+            readbuffer += conn.read(conn.in_waiting).decode('utf-8').replace('\x00','')
+            readout = readbuffer.split('\r\n')[:-1]
+            mean_update(adder(readout))
+            readbuffer = readbuffer.replace('\r\n'.join(readout),'')[2:]
+            print(len(readbuffer))
+except Exception as e:
+    print('[reader]' + str(e))
+    sys.exit()
