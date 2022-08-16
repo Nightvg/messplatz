6e658d2a4aecb352aade56a58aa79d9cdde440ba
Tobias Allrich <tobias@arbeit.de>
2022-08-16 15:05:53 +0200

    Added middleware as node-server Added plain html as template Changed bytereader to read in 30 chunks per second Added plotter.js for frontend structures

--- bytereader.py
+++ bytereader.py
@@ -2,31 +2,35 @@ import serial
 import struct
 import numpy as np
 from datetime import datetime
-
-datetime.now()
+from time import sleep
+import json
+import socket
 
 def xtof(arg) -> list:
-    return [float(struct.unpack('f',arg[start:stop])[0]) for start,stop in zip([0,4,8,12,16],[4,8,12,16,20])] if len(arg) == 20 else [arg]
+    return [float(struct.unpack('f',arg[start:stop])[0]) for start,stop in zip([0,4,8,12,16],[4,8,12,16,20])] if len(arg) == 20 else [0,0,0,0,0]
 
 file = open('test.txt','w')
 conn = serial.Serial(port='COM3', baudrate=500000, timeout=0.01)
 conn.set_buffer_size(rx_size=8192, tx_size=1024)
 readbuff = b''
+sock = websockets.connect('ws://127.0.0.1:8080')
 s = datetime.now()
 e = s
-i = 0
 bytesize = 0
 linesize = 0
 conn.flushInput()
-while conn.is_open and (e-s).total_seconds() < 1:
+while conn.is_open and (e-s).total_seconds() < 5:
     readbuff += conn.read_all()
     bytesize += len(readbuff)
-    readout = np.array([xtof(x) for x in readbuff.split(b'\r\n')[:-1]]) if i > 0 else np.array([xtof(x) for x in readbuff.split(b'\r\n')[1:-1]])
+    readout = np.array([xtof(x) for x in readbuff.split(b'\r\n')[:-1]])
     linesize += readout.shape[0]
     writeout = '\n'.join([','.join(x) for x in readout.astype(str)]) + '\n'
+    sock.send(json.dumps(np.mean(readout, axis=1)))
+    
     if len(writeout) > 1:
         file.write(writeout)
-        readbuff = readbuff[readbuff.rindex(b'\r\n'):] 
+        readbuff = readbuff[readbuff.rindex(b'\r\n')+2:] 
+    sleep(0.033)
     e = datetime.now()
 
 file.close()
---
+++ node-server/index.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const ws = require('ws');
+const app = express();
+
+const wsServer = new ws.Server({noServer: true});
+wsServer.on(
+    'connection', socket => {
+        socket.on('message', message => console.log(message));
+    }
+);
+
+const server = app.listen(8080);
+
+server.get('/', (req, res) => {
+  res.send()
+});
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+      wsServer.emit('connection', socket, request);
+    });
+  });
